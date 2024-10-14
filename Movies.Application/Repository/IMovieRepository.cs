e8185a4bfb993e19c1dd94d160481015f96f24ac
Ahmed Shaban <ahmed.shaban.ext@siemens.com>
2024-10-14 17:48:59 +0300

    Add IMovieRepository.

---
+++ Movies.Application/Repository/IMovieRepository.cs
@@ -0,0 +1,18 @@
+﻿using Movies.Application.Models;
+
+namespace Movies.Application.Repository;
+
+public interface IMovieRepository
+{
+    Task<IEnumerable<Movie>> GetAllAsync();
+    
+    Task<Movie?> GetByIdAsync(Guid id);
+    
+    Task<bool> CreateAsync(Movie movie);
+    
+    Task<bool> UpdateAsync(Movie movie);
+
+    Task<bool> DeleteByIdAsync(Guid id);
+    
+    // TODO: Should we have delete all?
+}
